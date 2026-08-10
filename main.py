8bb61e5a463c13ff4a3ce3d5d7ff3137eec82034
VenyaRai <venyarai18@gmail.com>
2026-08-10 21:38:07 +0530

    Add cafe management system

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def display_menu():
 def take_orders():
     global total
     while True:
-        order= input("\nEnter item name: ").title()
+        order=input("\nEnter item name: ").title()
         if order in menu:
             while True:
                 try:
@@ -40,31 +40,31 @@ def take_orders():
             print(f"\n{order} added successfully!")
             print(f"Current Total: Rs.{total}")
         else:
-            print("Sory! Item is unavailable.")
+            print("Sorry! Item is unavailable.")
             continue
         choice=input("\nDo you want to order more? (Yes/No): ").lower()
         if choice!="yes":
             break
 def print_bill():
-    bill_no = random.randint(1000, 9999)
-    now = datetime.now()
+    bill_no=random.randint(1000, 9999)
+    now=datetime.now()
     print("\n")
-    print("=" * 55)
+    print("="*55)
     print("               FINAL BILL")
-    print("=" * 55)
+    print("="*55)
     print(f"Bill No : {bill_no}")
     print(f"Date    : {now.strftime('%d-%m-%Y')}")
     print(f"Time    : {now.strftime('%I:%M %p')}")
-    print("-" * 55)
+    print("-"*55)
     print(f"{'Item':<22}{'Qty':<10}{'Price'}")
-    print("-" * 55)
+    print("-"*55)
     for item, qty, price in orders:
         print(f"{item:<22}{qty:<10}Rs.{price}")
-    print("-" * 55)
+    print("-"*55)
     print(f"{'TOTAL AMOUNT':<32}Rs.{total}")
-    print("=" * 55)
+    print("="*55)
     print("      Thank You! Visit Again ")
-    print("=" * 55)
+    print("="*55)
 display_menu()
 take_orders()
 print_bill()
